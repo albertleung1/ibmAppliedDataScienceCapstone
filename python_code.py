390b33e6dcd69b3d0564850724a9fc73cbf47286
Albert Leung <albert_leung_leung@yahoo.com>
2020-07-18 19:08:50 -0400

    Complete the Week 1 assignment

--- python_code.py
+++ python_code.py
@@ -1,2 +1,10 @@
-print("Hello World")
+#######################################################################################
+# This notebook will be mainly used for the IBM Applied Data Science Capstone project #
+#######################################################################################
+
+import pandas as pd
+import Numpy as np
+
+print('Hello Capstone Project Course!')
+
 
